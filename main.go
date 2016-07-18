7a7b2a098d8bbb5547aa6c847f00946c3a7f703f
Nick <nbdavis13@gmail.com>
2016-07-17 17:44:55 -0700

    Adds router

--- main.go
+++ main.go
@@ -2,20 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
+	//"database/sql"
+	//_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:@/user")
-	if err != nil {
-		fmt.Println("my sql error", err)
-	}
+	//db, err := sql.Open("mysql", "root:@/user")
+	//if err != nil {
+	//	fmt.Println("my sql error", err)
+	//}
 
-	_, err = db.Query("INSERT into MyGuests (firstname, lastname, email) VALUES ('nick', 'davis', 'nick.davis@sendgrid.com')")
-	if err != nil {
-		fmt.Println("insert error: ", err)
-	}
-	fmt.Println("Hello, world.\n")
+	//_, err = db.Query("INSERT into MyGuests (firstname, lastname, email) VALUES ('nick', 'davis', 'nick.davis@sendgrid.com')")
+	//if err != nil {
+	//	fmt.Println("insert error: ", err)
+	//}
+	r := mux.NewRouter()
+	r.HandleFunc("/signup", Signup)
+	log.Fatal(http.ListenAndServe(":50000", r))
+}
+
+func Signup(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("In Signup")
 }
